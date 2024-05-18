4bbd5675c8fb8fb8513e46c5a58c1458ff55c848
JossyFQ <147462438+JossyFQ@users.noreply.github.com>
2024-05-18 06:28:06 -0500

    Add files via upload

---
+++ registro-tratamiento.js
@@ -0,0 +1,21 @@
+function guardar() {
+
+    var nombre = document.getElementById("nombre").value;
+    var procedimientos = document.getElementById("procedimientos").value;
+    var profesional = document.getElementById("profesional").value;
+    var codigoPatologia = document.getElementById('codigoPatologia').value;
+    var codigoTratamiento = document.getElementById("codigoTratamiento").value;
+    var protocolo = document.getElementById("protocolo").value;
+    var fecha = document.getElementById("fecha").value;
+
+    // Validar que todos los campos estén completos
+    if (nombre.trim() === '' || procedimientos.trim() === '' || profesional.trim() === '' || codigoPatologia.trim() === '' || codigoTratamiento.trim() === '' || protocolo.trim() === '' || fecha.trim() === '') {
+        alert("Por favor complete todos los campos.");
+        return false;
+    }
+
+    // En este ejemplo, simplemente mostramos un mensaje de éxito
+    alert("Datos guardados");
+    return true;
+
+}
